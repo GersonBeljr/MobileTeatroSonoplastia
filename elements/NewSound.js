bdc7ab6f48b975d9060176f0a0668b569d651ad0
unknown <GersonBelJr>
2025-08-12 19:10:54 -0300

    commit a distancia pelo notebook

--- elements/NewSound.js
+++ elements/NewSound.js
@@ -1,9 +1,14 @@
 import { useState } from 'react';
 import {Image, TouchableOpacity} from 'react-native';
+import { useAudioPlayer } from 'expo-audio';
 
 function NewSound() {
+   
+
     return (
-        <TouchableOpacity className="border-4 rounded-xl border-[#FC822D] p-6 w-1/5 flex-row justify-center">
+        <TouchableOpacity 
+            className="border-4 rounded-xl border-[#FC822D] p-6 w-1/5 flex-row justify-center"
+            >
             <Image source={require('../assets/ICONS/addOrange.png')}  className="w-10 h-10 self-center"/>
         </TouchableOpacity>  
     )
--- elements/SoundBtn.js
+++ elements/SoundBtn.js
@@ -1,11 +1,32 @@
 import { useState } from 'react';
 import {TouchableOpacity, Text} from 'react-native';
 
+let activationColor = "";
+let defaultColor = "";
+
 function SoundBtn({boxTxt}) {
-    const  [t1, setName] = useState("");
+
+    const audioSource = require('../elements/SONS_TESTE/teste1.mp3');
+    const player = useAudioPlayer(audioSource);
+    let playing = false;
+
+    const handlePress = () =>{
+        playing = !playing;
+
+        if (playing) {
+            player.play();
+        } else {
+            player.pause();
+        }
+
+        player.seekTo(0);
+    };
 
     return (
-        <TouchableOpacity className="border-4 rounded-xl border-[#FC822D] justify-center p-6 w-1/5">
+        <TouchableOpacity 
+            className="border-4 rounded-xl border-[#FC822D] justify-center p-6 w-1/5"
+            onPress={handlePress}
+            >
             <Text className="text-xl text-center text-[#FC822D]">{boxTxt}</Text>
         </TouchableOpacity>    
     )
